d3df7649a671c907a41799d3212350302f1e5418
MohammadKiblawi <moudikiblawi630@gmail.com>
2022-03-26 22:41:18 +0200

    adding new files

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,12 +15,12 @@
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -29,45 +29,64 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <div class="my-3 form-check">
+                                    <input type="checkbox" onclick="showPassword()" class="form-check-input" id="exampleCheck1">
+                                    <label class="form-check-label" for="exampleCheck1">Show Password</label>
+                                </div>
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                        <div class=" row buttons justify-content-center align-items-center p-5">
+                            <div class="row justify-content-center">
+                                <div class="col-md-3 btn-primary button btn me-2 mb-3 lh-lg py-4">1</div>
+                                <div class="col-md-3 btn-primary button btn me-2 mb-3 lh-lg py-4">2</div>
+                                <div class="col-md-3 btn-primary button btn me-2 mb-3 lh-lg py-4">3</div>
                             </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                            <div class="row justify-content-center">
+                                <div class="col-md-3 btn-primary button btn me-2 mb-3 lh-lg py-4">4</div>
+                                <div class="col-md-3 btn-primary button btn me-2 mb-3 lh-lg py-4">5</div>
+                                <div class="col-md-3 btn-primary button btn me-2 mb-3 lh-lg py-4">6</div>
                             </div>
+                            <div class="row justify-content-center">
+                                <div class="col-md-3 btn-primary button btn me-2 mb-3 lh-lg py-4">7</div>
+                                <div class="col-md-3 btn-primary button btn me-2 mb-3 lh-lg py-4">8</div>
+                                <div class="col-md-3 btn-primary button btn me-2 mb-3 lh-lg py-4">9</div>
+                            </div>
+                            <div class="row col-md-3 py-4"><button type="submit" class="btn btn-success py-3">Login</button></div>
+
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+
 </div>
+<script>
+    let display = document.getElementById("password");
+    let buttons = Array.from(document.getElementsByClassName('button'));
+    buttons.map(button => {
+        button.addEventListener('click', (e) => {
+
+            display.value += e.target.innerHTML;
+            console.log(display.innerText);
+
+        });
+    });
+
+    function showPassword() {
+
+        if (display.type === "password") {
+            display.type = "text";
+        } else {
+            display.type = "password";
+        }
+    }
+</script>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,42 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+        <div class="col">
+            <a class="btn btn-primary float-end" href="#">Add Order</a>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Order Id</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Data/Time</th>
+                        <th scope="col">Options</th>
+                        <th scope="col">Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td><a class="btn btn-success rounded-pill">Done</a></td>
+                        <td>10-12-2022</td>
+                        <td></td>
+                        <td>20 $</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td><a class="btn btn-warning rounded-pill">Pending</a></td>
+                        <td>10-12-2022</td>
+                        <td></td>
+                        <td>20 $</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td><a class="btn btn-primary rounded-pill">Under Process</a></td>
+                        <td>10-12-2022</td>
+                        <td></td>
+                        <td>20 $</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
