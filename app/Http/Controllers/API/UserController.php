1dcdb1788dc75c4d6a87f65380056e9862478d33
MohammadKiblawi <moudikiblawi630@gmail.com>
2022-03-29 16:26:47 +0300

    adding new files

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+}
